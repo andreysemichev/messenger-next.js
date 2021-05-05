5299a5a82a9068a67fce92de8800f26aac26b50f
andreysemichev <andreysemichev@icloud.com>
2021-05-06 01:32:19 +0400

    Connect registration components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,26 @@
 import Welcome from "components/registration/Welcome";
+import EnterUsername from "components/registration/EnterUsername";
+import Twitter from "components/registration/Twitter";
+import ChooseAvatar from "components/registration/ChooseAvatar";
+import EnterPhone from "components/registration/EnterPhone";
+import EnterCode from "components/registration/EnterCode";
+import { useState } from "react";
+
+const stepsComponents = {
+    0: Welcome,
+    1: EnterUsername,
+    2: Twitter,
+    3: ChooseAvatar,
+    4: EnterPhone,
+    5: EnterCode,
+};
 
 function Home() {
+    const [step, setStep] = useState<number>(5);
+    const Step = stepsComponents[step];
+
     return (<>
-        <Welcome />
+        <Step />
     </>);
 }
 
